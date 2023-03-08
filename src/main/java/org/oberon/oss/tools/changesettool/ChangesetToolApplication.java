c2d57da9890f357b327caf8e2490b2ad0cbbbc7b
Fabien H. Dumay <fhdumay@oberon-oss.net>
2023-03-08 22:53:57 +0100

    Initial commit

---
+++ src/main/java/org/oberon/oss/tools/changesettool/ChangesetToolApplication.java
@@ -0,0 +1,13 @@
+package org.oberon.oss.tools.changesettool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangesetToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangesetToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/oberon/oss/tools/changesettool/ChangesetToolApplicationTests.java
@@ -0,0 +1,13 @@
+package org.oberon.oss.tools.changesettool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangesetToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
